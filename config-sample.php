a3766160210d7057699b02f921f9222f934890be
Oguz Gelal <o.gelal77@gmail.com>
2015-04-09 20:26:12 +0300

    First commit

---
+++ config-sample.php
@@ -0,0 +1,34 @@
+<?php
+/*
+* THE REAL CONFIG FILE WAS IGNORED BECAUSE IT CONTAINS PASSWORDS.
+* THIS FILE IS THE EXACT COPY OF THE ONE THAT IS USED, WITHOUT THE PASSWORDS.
+*/
+/* Change to false while deploying to a server */
+$local = true;
+
+session_name('oguzgelal');
+if(session_id() == ''){ session_start(); }
+
+# DB Settings
+if ($local){
+	define("DB_NAME", "");
+	define("DB_USER", "");
+	define("DB_PASSWORD", "");
+	define("DB_HOST", "");
+}
+else {
+	define("DB_NAME", "");
+	define("DB_USER", "");
+	define("DB_PASSWORD", "");
+	define("DB_HOST", "");
+}
+
+# PATH settings
+define("ROOT", "/");
+define("VIEWS", "frontend");
+
+# DOMAIN Settings
+define("PREFIX", "http://");
+if ($local){ define( "DOMAIN", "localhost:8888" ); }
+else{ define( "DOMAIN", "yourdomain.com" ); }
+
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'config.php';
+
+$app = new \Slim\Slim;
+
