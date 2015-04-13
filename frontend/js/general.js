e8ece7863d5c00163a77f31ab47048df9ffefd23
Oguz Gelal <o.gelal77@gmail.com>
2015-04-13 23:30:58 +0300

    window resize event bugs for safari fixed

--- frontend/js/general.js
+++ frontend/js/general.js
@@ -1,15 +1,29 @@
+
 $(document).ready(function(){
 	
 	fitPageWithSidepanel();
-	$(window).on("resize", function(){ fitPageWithSidepanel(); });
-	
+	// workaround to fix the race condition on safari
+	setTimeout(function(){ fitPageWithSidepanel(); },100);
+	$(window).on("resize", function(){
+		fitPageWithSidepanel();
+		// workaround for safari window resize timing issue
+		delay(function(){ fitPageWithSidepanel(); }, 100);
+	});
+
 });
 
 
-function fitPageWithSidepanel(){
-	var sidebar = $('.sidepanel');
+var fitPageWithSidepanel = function(){
+	var sidepanel = $('.sidepanel');
 	var page = $('.page');
-
-	var setWidth = $(window).width() - sidebar.width();
+	var setWidth = $(window).width() - sidepanel.width();
 	page.width(setWidth);
 }
+
+var delay = (function(){
+	var timer = 0;
+	return function(callback, ms){
+		clearTimeout (timer);
+		timer = setTimeout(callback, ms);
+	};
+})();
