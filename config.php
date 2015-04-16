0a41775ba907097ba4589d3f3d11865f131b8510
Oguz Gelal <o.gelal77@gmail.com>
2015-04-16 17:15:59 +0300

    local false fix

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 # Change to false while deploying to the server
-define("LOCAL", true);
+define("LOCAL", false);
 # True compiles less on the fly.
 # False loads the pre-compiled css at /frontend/assets/style/style.min.css
-define("COMPILE_LESS", true);
+define("COMPILE_LESS", false);
 
 session_name('oguzgelal');
 if(session_id() == ''){ session_start(); }
