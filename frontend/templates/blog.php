0bef6a11dc8890d1cef4ee704e86f3aa58f72532
Oguz Gelal <o.gelal77@gmail.com>
2015-04-18 22:21:36 +0300

    blog page design improved

--- frontend/templates/blog.php
+++ frontend/templates/blog.php
@@ -3,6 +3,7 @@
 		
 		<div class="blog-post">
 			<div class="blog-post-img">
+				<div class="blog-post-title">Lorem ipsum dolor sit amet</div>
 				<img src="http://lorempixel.com/1920/1920/people" alt="Dummy">
 			</div>
 		</div>
@@ -10,30 +11,35 @@
 		<div class="blog-post">
 			<div class="blog-post-img">
 				<img src="http://lorempixel.com/1920/1920/abstract" alt="Dummy">
+				<div class="blog-post-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
 			</div>
 		</div>
 
 		<div class="blog-post">
 			<div class="blog-post-img">
 				<img src="http://lorempixel.com/1920/1920/city" alt="Dummy">
+				<div class="blog-post-title">Lorem ipsum dolor</div>
 			</div>
 		</div>
 
 		<div class="blog-post">
 			<div class="blog-post-img">
 				<img src="http://lorempixel.com/1920/1920/animals" alt="Dummy">
+				<div class="blog-post-title">Eos sed, sunt adipisci aliquid natus nulla beatae debitis!</div>
 			</div>
 		</div>
 
 		<div class="blog-post">
 			<div class="blog-post-img">
 				<img src="http://lorempixel.com/1920/1920/nature" alt="Dummy">
+				<div class="blog-post-title">Et totam quidem debitis qui placeat architecto fugit quae. Asperiores ab, maiores assumenda!</div>
 			</div>
 		</div>
 
 		<div class="blog-post">
 			<div class="blog-post-img">
 				<img src="http://lorempixel.com/1920/1920/sports" alt="Dummy">
+				<div class="blog-post-title">Lorem ipsum dolor sit amet</div>
 			</div>
 		</div>
 
