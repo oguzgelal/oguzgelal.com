469c78db1578f3dc76a1632b1c80682a8cfc80aa
Oguz Gelal <o.gelal77@gmail.com>
2015-04-19 19:59:29 +0300

    blogpost page design improvement

--- frontend/templates/blogpost.php
+++ frontend/templates/blogpost.php
@@ -18,7 +18,7 @@ else {
 <div class="page blogpost">
 	<div class="blogpost-cover" style="background-image: url(<?php echo BLOG.$post["cover"]; ?>); background-size: auto auto; background-position: center;;">
 		<div class="blogpost-posttime"><i class="fa fa-calendar-o"></i> <?php echo $readableTime; ?></div>
-		<div class="blogpost-title fancy-enter-bounce"><?php echo $post["title"]; ?></div>
+		<div class="blogpost-title animated tada"><?php echo $post["title"]; ?></div>
 	</div>
 	<div class="blogpost-wrapper">
 		<div class="blogpost-row blogpost-infotiles" style="margin-top: 30px;">
