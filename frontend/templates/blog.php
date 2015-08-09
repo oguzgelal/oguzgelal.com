7fb0c1bbb2f14728edea5f2c89505f04cd8f3876
Oguz Gelal <o.gelal77@gmail.com>
2015-08-10 02:20:27 +0300

    out link for cover images available for blog

--- frontend/templates/blog.php
+++ frontend/templates/blog.php
@@ -21,11 +21,20 @@
 				$postLink = "/blog/post/".$post['id'];
 				$postTarget = "_self";
 			}
+
+			$postImgurl = BLOG.$post['cover'];
+			if(isset($post['out_imgurl']) && $post['out_imgurl']!=""){ $postImgurl = $post['out_imgurl']; }
+			
 			?>
-			<div class="blog-post" id="<?php echo $post['id']; ?>" data-img="<?php echo BLOG.$post['cover']; ?>">
+			<div class="blog-post" id="<?php echo $post['id']; ?>" data-img="<?php echo $postImgurl; ?>">
 				<a href="<?php echo $postLink; ?>" target="<?php echo $postTarget; ?>">
 					<div class="blog-post-hover">
-						<div class="blog-post-lang"><i class="fa fa-calendar-o" style="position: absolute; margin-top: -2px;"></i> <span style="margin-left: 15px;"><?php echo $readableTime; ?></span> <i class="fa fa-globe" style="font-size: 14px; position: absolute; margin-top: -3px; margin-left: 3px;"></i> <img style="margin-left: 19px;" class="blog-post-lang-flag" src="<?php echo FLAGS.$post['language'].".png"; ?>" alt="US Flag"></div>
+						<div class="blog-post-lang">
+							<i class="fa fa-calendar-o" style="position: absolute; margin-top: -2px;"></i>
+							<span style="margin-left: 15px;"><?php echo $readableTime; ?></span>
+							<i class="fa fa-globe" style="font-size: 14px; position: absolute; margin-top: -3px; margin-left: 3px;"></i>
+							<img style="margin-left: 19px;" class="blog-post-lang-flag" src="<?php echo FLAGS.$post['language'].".png"; ?>" alt="US Flag">
+						</div>
 						<div class="blog-post-title"><?php echo $post['title']; ?></div>
 					</div>
 				</a>
