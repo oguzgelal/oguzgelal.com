756a2dd753bf9c7248749b8bcd17351982985e56
Oguz Gelal <o.gelal77@gmail.com>
2015-08-10 02:16:41 +0300

    out link available for blog posts

--- frontend/templates/blog.php
+++ frontend/templates/blog.php
@@ -10,9 +10,20 @@
 			$date = new DateTime($post["posttime"]);
 			$timestamp = $date->getTimestamp();
 			$readableTime = date("M jS, Y", $timestamp);
+
+			$postLink = "#";
+			$postTarget = "_self";
+			if (isset($post['out_link']) && $post['out_link']!=""){
+				$postLink = $post['out_link'];
+				$postTarget = "_new";
+			}
+			else{
+				$postLink = "/blog/post/".$post['id'];
+				$postTarget = "_self";
+			}
 			?>
 			<div class="blog-post" id="<?php echo $post['id']; ?>" data-img="<?php echo BLOG.$post['cover']; ?>">
-				<a href="/blog/post/<?php echo $post['id']; ?>">
+				<a href="<?php echo $postLink; ?>" target="<?php echo $postTarget; ?>">
 					<div class="blog-post-hover">
 						<div class="blog-post-lang"><i class="fa fa-calendar-o" style="position: absolute; margin-top: -2px;"></i> <span style="margin-left: 15px;"><?php echo $readableTime; ?></span> <i class="fa fa-globe" style="font-size: 14px; position: absolute; margin-top: -3px; margin-left: 3px;"></i> <img style="margin-left: 19px;" class="blog-post-lang-flag" src="<?php echo FLAGS.$post['language'].".png"; ?>" alt="US Flag"></div>
 						<div class="blog-post-title"><?php echo $post['title']; ?></div>
