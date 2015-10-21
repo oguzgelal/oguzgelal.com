b5e10594ce702f6437cc55c6547fc4d5c3af39eb
Oguz Gelal <o.gelal77@gmail.com>
2015-10-22 00:32:42 +0300

    home update

--- frontend/templates/home.php
+++ frontend/templates/home.php
@@ -11,29 +11,23 @@
 		<div class="homepage-row homepage-infotiles fancy-enter-bounce" style="margin-top: 30px;">
 			<p>
 				I'm a 
-				<span class="tooltip" title="Yeah, of course this is dynamic. <br> What, you thought I update this every year ?"> <?php echo date("Y")-1993; ?> </span> year old programmer, entrepreneur, geek, 
-				<a href="http://ind.ie" target="_new">open-source enthusiast</a> and the founder of <a href='https://socialsurf.io' target="_new">Social Surf</a>. I enjoy watching movies 
-				<span class="desc-comment">(comedy, horror and drama)</span> 
-				and reading books. 
-				I used to draw <a href="http://ctp200.com" target="_new">webcomics</a>, I love music, 
-				especially classical and classical rock. I play the piano and the drums.
+				<span class="tooltip" title="Yeah, of course this is dynamic."> <?php echo date("Y")-1993; ?> </span> year old programmer, entrepreneur, geek and an engineer at <a href="http://www.toptal.com/resume/oguz-gelal" target="_new">Toptal</a>. 
+				I enjoy watching movies <span class="desc-comment">(mostly comedy and horror)</span> and reading books. 
+				My favorites are as follows: <a href="http://www.imdb.com/title/tt0150742/" target="_new">Godfather Triology</a> as movie, <a href="https://en.wikipedia.org/wiki/George_Carlin" target="_new">George Carlin</a> as a comedian, <a href="http://www.imdb.com/title/tt0108778/" target="_new">Friends</a> and <a href="http://www.imdb.com/title/tt0182576/" target="_new">Family Guy</a> as TV shows, <a href="https://en.wikipedia.org/wiki/George_Orwell" target="_new">George Orwell</a> as author.
+				I love music, especially classical rock. I don't have a favorite band though.
+				I used to draw and publish my own <a href="http://ctp200.com" target="_new">webcomics</a>. I play the piano and the drums.
 			</p>
 		</div>
 		<div class="homepage-row homepage-infotiles fancy-enter-bounce">
 			<p>
-				I was born in Izmir and moved to Istanbul for
-				<span class="tooltip tooltip-highlight" title="Ozyegin University">college</span>. 
-				<!-- I had many work experiences and took quite a few responsibilities, 
-				you can find more about those <a href="/cv" target="_self">here</a>.-->
-				My field of interests are web, computer networks and 
-				<a href='http://en.wikipedia.org/wiki/Semantic_Web' target='_new'>semantic web technologies</a>. 
-				As a web developer (frontend and back-end) I used to work as a <a href="https://www.fiverr.com/oguzgelal/" target="_new">freelancer</a> 
-				and for an awesome startup <a href="http://cubic.fm" target="_new">cubic.fm</a> 
-				as a frontend developer. Now, I'm working at <a href="http://webrazzi.com" target="_new">Webrazzi</a> as a software developer & <a href='https://en.wikipedia.org/wiki/Entrepreneur_in_Residence' target='_new'>entrepreneur in residence</a> (developing <a href='https://socialsurf.io' target="_new">SocialSurf</a>). Other than that, I have some 
-				<a href="/portfolio" target="_self">ongoing projects</a> too. 
-				If you have any questions or comments, please don't hesitate 
-				to <a href="/contact" target="_self">contact me</a> and I'll 
-				be glad to get back to you. Thanks for visiting my website.</p>
-			</div>
+				I was born in Izmir and moved to Istanbul for <span class="tooltip tooltip-highlight" title="Ozyegin University">college</span>. 
+				I had many work experiences and took quite a few responsibilities, you can find more about those <a href="/cv" target="_self">here</a>.
+				My field of interests are web, computer networks and <a href='http://en.wikipedia.org/wiki/Semantic_Web' target='_new'>semantic web technologies</a>. 
+				As for my professional career, I work as a freelance web developer (frontend and back-end) at Toptal. <a href="http://www.toptal.com/resume/oguz-gelal" target="_new">Click here</a> to visit my Toptal profile here.
+				Other than that, I have some <a href="/portfolio" target="_self">ongoing projects</a> too. 
+				If you have any questions or comments, please don't hesitate to <a href="/contact" target="_self">contact me</a> and I'll 
+				be glad to get back to you. Thanks for visiting my website.
+			</p>
 		</div>
 	</div>
+</div>
